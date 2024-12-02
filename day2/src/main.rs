53a78ee3ba3b8740bf6f2fdde8d63253f7aba7c0
Lesley Lai <lesley@lesleylai.info>
2024-12-02 21:51:39 +1100

    Day 2 small refactoring

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -8,12 +8,19 @@ const TEST_INPUT: &str = "7 6 4 2 1
 const INPUT: &str = include_str!("./input.txt");
 
 fn is_safe(report: &[i32]) -> bool {
-    report
-        .windows(2)
-        .all(|w| w[1] < w[0] && w[0] - w[1] >= 1 && w[0] - w[1] <= 3)
-        || report
-            .windows(2)
-            .all(|w| w[1] > w[0] && w[1] - w[0] >= 1 && w[1] - w[0] <= 3)
+    let mut differences = report.windows(2).map(|w| w[1] - w[0]);
+    differences.clone().all(|d| d >= 1 && d <= 3) || differences.all(|d| d <= -1 && d >= -3)
+}
+
+fn is_safe_tolerate(report: &[i32]) -> bool {
+    for i in 0..report.len() {
+        let mut report_clone = report.to_vec();
+        report_clone.remove(i);
+        if is_safe(&report_clone) {
+            return true;
+        }
+    }
+    false
 }
 
 fn parse(input: &str) -> Vec<Vec<i32>> {
@@ -28,26 +35,13 @@ fn part1(input: &str) -> usize {
     reports.iter().filter(|report| is_safe(&report)).count()
 }
 
-fn part2(input: &str) -> i32 {
+fn part2(input: &str) -> usize {
     let reports = parse(input);
 
-    let mut count = 0;
-    for report in reports {
-        if is_safe(&report) {
-            count += 1;
-        } else {
-            for i in 0..report.len() {
-                let mut numbers_clone = report.clone();
-                numbers_clone.remove(i);
-                if is_safe(&numbers_clone) {
-                    count += 1;
-                    break;
-                }
-            }
-        }
-    }
-
-    count
+    reports
+        .iter()
+        .filter(|report| is_safe(&report) || is_safe_tolerate(&report))
+        .count()
 }
 
 fn main() {
